3b898d3c78a2a94476f7226c4354ef868dff0b8f
Quanwen Chen <chenquanwen1677@gmail.com>
2020-08-05 19:24:30 +0800

    Add dropdown selection for Year1 Remove redundant files

--- nottingtable/front_page/views.py
+++ nottingtable/front_page/views.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
 from flask import render_template
 
+from nottingtable.crawler.models import Y1Group
+
 bp = Blueprint('front', __name__, template_folder='templates')
 
 
@@ -11,7 +13,13 @@ def index():
 
 @bp.route('/year-1', methods=('GET',))
 def show_year_1():
-    return render_template('year-1.html')
+    """
+    Render page for year1 selection page
+    :return: rendered page with selection list
+    """
+
+    group_list = Y1Group.query.all()
+    return render_template('year-1.html', group_list=group_list)
 
 
 @bp.route('/public-api', methods=('GET',))
