dbdea7f8c90f95ed5d0e0161bf7e063822bc7350
Quanwen Chen <chenquanwen1677@gmail.com>
2020-08-06 12:53:09 +0800

    Add bootstrap card to frontend

--- nottingtable/front_page/views.py
+++ nottingtable/front_page/views.py
@@ -11,6 +11,10 @@ bp = Blueprint('front', __name__, template_folder='templates')
 def index():
     return render_template('index.html')
 
+@bp.route('/year-24', methods=('GET',))
+def show_year_24():
+    return render_template('year-24.html')
+
 
 @bp.route('/year-1', methods=('GET',))
 def show_year_1():
