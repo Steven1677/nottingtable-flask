ed580814f177feccac17e58df238a178cd3ced6f
Quanwen Chen <chenquanwen1677@gmail.com>
2020-08-06 10:54:20 +0800

    Update config.py

--- nottingtable/config.py
+++ nottingtable/config.py
@@ -4,6 +4,12 @@ import arrow
 
 class Config(object):
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    BASE_URL = 'http://timetablingunnc.nottingham.ac.uk:8005/'
+    FIRST_MONDAY = arrow.get('2019-09-16')  # YYYY-MM-DD
+    YEAR1_PDF_URL = 'https://www.nottingham.edu.cn/en/academicservices/documents/year-1-student-timetable-1920-spring-semester-20200429v4.pdf'
 
 
 class ProductionConfig(Config):
@@ -18,6 +24,3 @@ class DevelopmentConfig(Config):
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    BASE_URL = 'http://timetablingunnc.nottingham.ac.uk:8005/'
-    FIRST_MONDAY = arrow.get('2019-09-16')  # YYYY-MM-DD
-    YEAR1_PDF_URL = 'https://www.nottingham.edu.cn/en/academicservices/documents/year-1-student-timetable-1920-spring-semester-20200429v4.pdf'
